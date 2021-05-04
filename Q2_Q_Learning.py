f133852b1d9934055a22f2b55ee1132a4baf428b
varundesai10 <ee1180511@iitd.ac.in>
2021-05-04 14:29:00 +0530

    fixed bugs q2 and plotted figures but need to plot figures again

--- Q2_Q_Learning.py
+++ Q2_Q_Learning.py
@@ -1,6 +1,11 @@
 import numpy as np
 import random
-from plotpolicy import plot_policy
+from plotpolicy import plot_policy, plot_policy2
+from simulatepolicy import plot_simulation, simulate_policy
+import argparse
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
 actions = {0:"north", 1:"south", 2:"east", 3:"west"}
 
 
@@ -18,7 +23,7 @@ def Environment_interaction_next_state_reward(s_t, action, goal_state):
     elif actions[action] == "east":
         actual_action = np.random.choice([0, 1, 2, 3], p=[np.float(0.2 / 3), np.float(0.2 / 3), 0.8, np.float(0.2 / 3)])
     elif actions[action] == "west":
-        actual_action = np.random.choice([0, 1, 2, 3], p=[np.float(0.2 / 3), np.float(0.2 / 3), 0.8, np.float(0.2 / 3)])
+        actual_action = np.random.choice([0, 1, 2, 3], p=[np.float(0.2 / 3), np.float(0.2 / 3), np.float(0.2 / 3), 0.8])
 
     next_state = None
     if actual_action == 0:
@@ -57,6 +62,8 @@ def epsilon_greedy(epsilon,Q_values):
 
 def Q_learning_algorithm(epsilon, alpha, gamma, goal_state, no_of_episodes):
     Q_value = {}
+    reward_history = []
+    
     for i in range(1,49):
         for j in range(1,24):
             if i in [25,26] and j in range(1,12) or i in [25,26] and j in range(13,24):
@@ -68,20 +75,23 @@ def Q_learning_algorithm(epsilon, alpha, gamma, goal_state, no_of_episodes):
                         Q_value[(i, j)][k] = 0
                     else:
                         Q_value[(i,j)][k] = np.random.normal(5, 20, size=(1))[0]
+                        #Q_value[(i,j)][k] = 0.0
 
-
-    for i in range(no_of_episodes):
+    for i in tqdm(range(no_of_episodes)):
         present_state = None
         while present_state == None:
             present_state= (random.randint(1,48),random.randint(1,23))
             if present_state[0] in [25,26] and present_state[1] in range(1,12) or present_state[0] in [25,26] and present_state[1] in range(13,24):
                 present_state = None
-        print("Episode running")
-        print(i+1)
+        reward_this_episode = 0
+        #print("Episode running")
+        #print(i+1)
+        
         while(True):
 
             current_action = epsilon_greedy(epsilon, Q_value[present_state])
             next_state, reward = Environment_interaction_next_state_reward(present_state,current_action,goal_state)
+            reward_this_episode += reward
             values = np.array([Q_value[next_state][0],Q_value[next_state][1],Q_value[next_state][2],Q_value[next_state][3]])
             #print(values)
             increment_1 = values[np.argmax(values)]
@@ -89,19 +99,42 @@ def Q_learning_algorithm(epsilon, alpha, gamma, goal_state, no_of_episodes):
             present_state = next_state
             if present_state[0] == goal_state[0] and present_state[1] == goal_state[1]:
                 break
-    return  Q_value
+        reward_history.append(reward_this_episode)
+    
+    return  Q_value, reward_history
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-e', '--episodes', help='no of episodes to run', type=int, default=4000)
+parser.add_argument('--eps', help='exploring probability', type=float, default=0.05)
+parser.add_argument('-a', '--alpha', help='learning rate', type=float, default=0.25)
+parser.add_argument('-g', '--gamma', help='discount factor', type=float, default=0.99)
 
-q_value = Q_learning_algorithm(0.05, 0.25,0.99,(48,12),4000)
+args = parser.parse_args()
+eps = args.eps
+episodes = args.episodes
+alpha = args.alpha
+gamma = args.gamma
+
+q_value, reward_history = Q_learning_algorithm(eps, alpha, gamma, (48,12), episodes)
 Policy3 = np.zeros((50,25))
-
+Value = np.zeros((50, 25))
 for ki in range(1,49):
     for kj in range(1,24):
         if ki in [25, 26] and kj in range(1, 12) or ki in [25, 26] and kj in range(13, 24):
-            continue
+            Value[ki, kj] == None
         else:
             Policy3[ki, kj] = np.argmax(np.array([q_value[(ki,kj)][0], q_value[(ki,kj)][1], q_value[(ki,kj)][2], q_value[(ki,kj)][3]]))
+            Value[ki, kj] = max(q_value[(ki, kj)][0], q_value[(ki, kj)][1], q_value[(ki, kj)][2], q_value[(ki, kj)][3])
 
+plt.imshow(Value.T)
+plt.title("Value function after {} episodes. [gamma = {}, eps={}, alpha={}]".format(episodes, gamma, eps, alpha))
+plt.show()
 
-Value = np.random.normal(5, 20, size=(50,25))
-plot_policy(Policy3.T, Value.T)
+plot_policy2(Policy3.T, Value.T, grid=False, title="Policy plot after {} episodes. [gamma = {}, eps={}, alpha={}]".format(episodes, gamma, eps, alpha))
+
+plt.plot(reward_history)
+plt.title("Reward History for {} episodes. [gamma = {}, eps={}, alpha={}]".format(episodes, gamma, eps, alpha))
+plt.show()
+
+states, actions, rewards = simulate_policy((1, 1), Policy3, iter=2000)
+plot_simulation(states, actions)
--- plotpolicy.py
+++ plotpolicy.py
@@ -16,7 +16,7 @@ def move(x, y, a):
     elif a == 3:
         return x - 1, y
 
-def plot_policy(P, V, orientation_dict = orientation_dict):
+def plot_policy(P, V, orientation_dict = orientation_dict, overlay=False):
     #P - policy, V - value function
     assert P.shape[0] == 25 and P.shape[1] == 50, "Improper input to the function"
     assert P.shape == V.shape, "Improper input to the function"
@@ -26,27 +26,60 @@ def plot_policy(P, V, orientation_dict = orientation_dict):
     for y in range(25):
         for x in range(50):
             #color the walls
-            
+    
             if(x > 0 and x < 49 and y > 0 and y < 24 and not (x in [25,26] and y in range(1,12) or x in [25,26] and y in range(13,24))):
+                if(overlay):
+                    marker = shapes.Rectangle((x, y), width=1, height=1, color=f'{V[y, x]}', alpha=0.5)
+                    ax.add_artist(marker)
                 x_, y_ = move(x, y, P[y, x])
                 if P[y, x] == 0:
-                    ax.arrow(x + 0.5, y, x_-x, y_-y, width=0.1, length_includes_head=True)
+                    ax.arrow(x + 0.5, y, x_-x, y_-y, width=0.1, length_includes_head=True, color='red')
                 elif P[y, x] == 1:
-                    ax.arrow(x + 0.5, y + 1, x_-x, y_-y, width=0.1, length_includes_head=True)
+                    ax.arrow(x + 0.5, y + 1, x_-x, y_-y, width=0.1, length_includes_head=True, color='red')
                 elif P[y, x] == 2:
-                    ax.arrow(x, y + 0.5, x_-x, y_-y, width=0.1, length_includes_head=True)
+                    ax.arrow(x, y + 0.5, x_-x, y_-y, width=0.1, length_includes_head=True, color='red')
                 elif P[y, x] == 3:
-                    ax.arrow(x + 1, y + 0.5, x_-x, y_-y, width=0.1, length_includes_head=True)
+                    ax.arrow(x + 1, y + 0.5, x_-x, y_-y, width=0.1, length_includes_head=True, color='red')
             else:
                 if(x == goal_state[1] and y == goal_state[0]):
                     marker = shapes.Rectangle((x, y), width=1, height=1, color='red')
                 else:    
-                    marker = shapes.Rectangle((x, y), width=1, height=1, color='black')
+                    marker = shapes.Rectangle((x, y), width=1, height=1, color='blue')
                 ax.add_artist(marker)
+    
     ax.set_xticks(np.arange(0, 50, 1))
     ax.set_yticks(np.arange(0, 25, 1))
     ax.grid(b=True, which='major')
     ax.set_title('Policy Plot')
+
+    plt.show()
+
+def plot_policy2(P, V, orientation_dict = orientation_dict, grid=False, title='Policy Plot'):
+    #P - policy, V - value function
+    assert P.shape[0] == 25 and P.shape[1] == 50, "Improper input to the function"
+    assert P.shape == V.shape, "Improper input to the function"
+    _, ax = plt.subplots()
+    ax.imshow(V)
+    for y in range(25):
+        for x in range(50):
+            #color the walls
+    
+            if(x > 0 and x < 49 and y > 0 and y < 24 and not (x in [25,26] and y in range(1,12) or x in [25,26] and y in range(13,24))):
+                x_, y_ = move(x, y, P[y, x])
+                if P[y, x] == 0:
+                    ax.arrow(x, y-0.5, x_-x, y_-y, width=0.1, length_includes_head=True, color='red')
+                elif P[y, x] == 1:
+                    ax.arrow(x, y + 0.5, x_-x, y_-y, width=0.1, length_includes_head=True, color='red')
+                elif P[y, x] == 2:
+                    ax.arrow(x-0.5, y, x_-x, y_-y, width=0.1, length_includes_head=True, color='red')
+                elif P[y, x] == 3:
+                    ax.arrow(x + 0.5, y, x_-x, y_-y, width=0.1, length_includes_head=True, color='red')
+    
+    ax.set_xticks(np.arange(0, 50, 1))
+    ax.set_yticks(np.arange(0, 25, 1))
+    if(grid):
+        ax.grid(b=True, which='major')
+    ax.set_title(title)
 
     plt.show()
     
